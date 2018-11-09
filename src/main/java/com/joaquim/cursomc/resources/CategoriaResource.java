e3399f53d96cc82924eee78dc5b32ec9214b3cf3
Rafael <rafaeljoaquim86@gmail.com>
2018-11-09 09:06:15 -0200

    adicionando classe categoria

--- src/main/java/com/joaquim/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/joaquim/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.joaquim.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.joaquim.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping (value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "TI");
+		Categoria cat2 = new Categoria(1, "Redes");
+		
+		List <Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 	
